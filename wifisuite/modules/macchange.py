24113004c0c9be123cbde6b811b2ef2cb9260e4a
nicksanzotta <nicksanzotta@outlook.com>
2017-08-17 19:06:12 -0400

    Cleaned up macchanger

--- wifisuite/modules/macchange.py
+++ wifisuite/modules/macchange.py
@@ -3,35 +3,26 @@
 # Author: Nick Sanzotta/@Beamr
 # Version: v 1.05162017
 
-import os
-import time
-# from wpa_supplicant.core import WpaSupplicantDriver
+import os, time
+from subprocess import Popen, PIPE 
 from theme import *
-
-def getMAC(interface):
-	wirelessInt = str(interface.get_ifname())
-	os.system('ifconfig ' + str(wirelessInt) +\
-	" | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'")
-	return
 
 def macRandom(interface):
 	wirelessInt = str(interface.get_ifname())
-	print('Current MAC Address: ')
-	getMAC(interface)
+	p1 = Popen(["ifconfig " + wirelessInt + " | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"], shell=True, stdout=PIPE)
+	print(blue('i') + 'Current MAC Address: %s' % (p1.communicate()[0].rstrip('\n')))
 	os.system('ifconfig ' + wirelessInt + ' down')
 	os.system('macchanger -r ' + wirelessInt + ' > /dev/null')
 	os.system('ifconfig ' + wirelessInt + ' up')
-	print(' \nNew MAC Address: ')
-	getMAC(interface)
-	print('\n')
+	p2 = Popen(["ifconfig " + wirelessInt + " | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"], shell=True, stdout=PIPE)
+	print(green('*') + 'New MAC Address: %s' % (p2.communicate()[0].rstrip('\n')))
 
 def macManual(interface, macaddress):
 	wirelessInt = str(interface.get_ifname())
-	print('Current MAC Address: ')
-	getMAC(interface)
+	p1 = Popen(["ifconfig " + wirelessInt + " | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"], shell=True, stdout=PIPE)
+	print(blue('i') + 'Current MAC Address: %s' % (p1.communicate()[0].rstrip('\n')))
 	os.system('ifconfig ' + wirelessInt + ' down')
 	os.system('macchanger -m ' + macaddress + ' ' + wirelessInt + ' > /dev/null')
 	os.system('ifconfig ' + wirelessInt + ' up')
-	print(' \nNew MAC Address: ')
-	getMAC(interface)
-	print('\n')
+	p2 = Popen(["ifconfig " + wirelessInt + " | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"], shell=True, stdout=PIPE)
+	print(green('*') + 'New MAC Address: %s' % (p2.communicate()[0].rstrip('\n')))
